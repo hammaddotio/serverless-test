5a04013ebd50f3da728d422bbbeb9845af2ac06c
Hammad <hammaddotio@gmail.com>
2024-05-20 08:21:58 -0700

    test

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const HOST = process.env.HOST || 'localhost'
 
+app.get('/', (req, res) => {
+    res.json({
+        response: 'OKY'
+    })
+})
+
+
+
 
 app.listen(PORT, HOST, () => console.log(`server listening on port ${PORT}`))
